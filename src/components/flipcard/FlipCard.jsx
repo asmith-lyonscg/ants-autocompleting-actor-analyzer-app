4b6787d29e35e479e811bb36397f49f803a96432
Anthony Smith <asmith@lyonscg.com>
2022-11-04 16:55:57 -0500

    pulled flip card CSS into its own css file, for better component portability

--- src/components/flipcard/FlipCard.jsx
+++ src/components/flipcard/FlipCard.jsx
@@ -1,5 +1,6 @@
 import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
+import "./flipcard.css";
 
 export const FlipCard = ( props ) => {
     if ( props.children !== undefined ) {
